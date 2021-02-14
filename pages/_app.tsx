a1146a5ceaab547f546d961e0714af098fc2a9b7
Manuel Alejandro da Costa <manuelalejandrodacostacontreras>
2021-02-14 13:31:54 +0100

    chore: added main page file, modified app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/styles.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/main/main.tsx
+++ pages/main/main.tsx
@@ -1,5 +1,5 @@
-import GoogleMap from '../../components/googleMaps/googleMap';
 import Layout from '../../components/Layout/layout';
+import GoogleMap from '../../components/googleMaps/googleMaps';
 
 // Move to a mock folder
 const initialLocation = { lat: 41.49675, lng: 1.898728 };
@@ -7,7 +7,9 @@ const initialLocation = { lat: 41.49675, lng: 1.898728 };
 export default function MainComponent() {
   return (
     <Layout>
-      <GoogleMap zoom={15} initialLocation={initialLocation} />
+      <div className="small-container">
+        <GoogleMap zoom={15} initialLocation={initialLocation} />
+      </div>
     </Layout>
   );
 }
