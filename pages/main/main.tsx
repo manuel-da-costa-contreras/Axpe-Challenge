66e835ded5851b91411892c54cdc4109abba68c6
Manuel Alejandro da Costa <manuelalejandrodacostacontreras>
2021-02-14 20:11:41 +0100

    feat: added main layout page

--- pages/main/main.tsx
+++ pages/main/main.tsx
@@ -1,15 +1,10 @@
 import Layout from '../../components/Layout/layout';
-import GoogleMap from '../../components/googleMaps/googleMaps';
-
-// Move to a mock folder
-const initialLocation = { lat: 41.49675, lng: 1.898728 };
+import GoogleMaps from '../../components/googleMaps/googleMaps';
 
 export default function MainComponent() {
   return (
     <Layout>
-      <div className="small-container">
-        <GoogleMap zoom={15} initialLocation={initialLocation} />
-      </div>
+      <GoogleMaps />
     </Layout>
   );
 }
