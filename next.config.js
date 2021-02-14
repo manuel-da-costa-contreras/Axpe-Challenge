9c1d8e5b9fb1d2f66b64398d921a560538ce0a10
Manuel Alejandro da Costa <manuelalejandrodacostacontreras>
2021-02-14 20:10:16 +0100

    chore: added libraries react google maps api, modified index to load main page

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const path = require('path')
 module.exports = {
     sassOptions: {
         includePath: [path.join(__dirname, 'styles')]
+    },
+    env: {
+        googleMapKey: 'AIzaSyB3EkGRUsuzb8JflnVg_v8wy2bamHZGGkI'
     }
 }
